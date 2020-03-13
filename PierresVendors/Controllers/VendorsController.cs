58e4ac8d4bee293fc87345932be26dae9d21c075
Patrick Kille <pr.kille@gmail.com>
2020-03-13 12:03:09 -0700

    correct routing and function calls

--- PierresVendors/Controllers/VendorsController.cs
+++ PierresVendors/Controllers/VendorsController.cs
@@ -7,31 +7,48 @@ namespace PierresVendors.Controllers
 {
   public class VendorsController : Controller
   {
-    [HttpGet("/vendors/{vendorsId}/orders/new")]
-    public ActionResult New(int id)
+    [HttpGet("/vendors")]
+    public ActionResult Index()
     {
-      Vendor vendor = Vendor.Find(id);
-      return View(vendor);
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
     }
 
-    [HttpGet("/vendors/{vendorsId}/orders/{orderId}")]
-    public ActionResult Show(int vendorID, int orderID)
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
     {
-      Order order = Order.Find(orderID);
-      Vendor vendor = Vendor.Find(vendorID);
-      Dictionary <string, object> model = new Dictionary<string, object>();
-      model.Add("order", order);
-      model.Add("vendor", vendor);
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string name, string description)
+    {
+      Vendor newVendor = new Vendor(name, description);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{vendorId}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
       return View(model);
     }
 
-    [HttpPost("/vendors/{vendorsId}/orders/{orderId}")]
-    public ActionResult Delete(int orderID)
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string title, string description, int price, string day, bool reoccurring)
     {
-      List<Order> orders = Order.GetAll();
-      int indexToDelete = orders.FindIndex(order => order.Id == orderID);
-      orders.RemoveAt(indexToDelete);
-      return RedirectToAction("Show", "Vendor");
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(title, description, price, day, reoccurring);
+      selectedVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("order", vendorOrders);
+      return View("Show", model);
     }
   }
 }
