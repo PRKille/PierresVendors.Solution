0f287a7f95e63b3a0105a8af9b3b1e03d06793aa
Patrick Kille <pr.kille@gmail.com>
2020-03-13 09:50:33 -0700

    add in vendor model

---
+++ PierresVendors/Models/Vendor.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+
+namespace PierresVendors.Models
+{
+  public class Vendor
+  {
+    private static List<Vendor> _vendors = new List<Vendor>{};
+    private static int _count = 0;
+    public string Name {get;set;}
+
+    public string Description {get;set;}
+    public int Id {get;}
+    public List<Order> Orders {get;set;}
+
+    public Vendor(string name, string description)
+    {
+      Name = name;
+      Description = description;
+      _vendors.Add(this);
+      Id = _count++;
+      Albums = new List<Album>{};
+    }
+
+    public static List<Vendor> GetAll()
+    {
+      return _vendors;
+    }
+
+    public static void ClearAll()
+    {
+      _vendors.Clear();
+    }
+
+    public static Vendor Find(int id)
+    {
+      int index = _vendors.FindIndex(artist => artist.Id == id);
+      return _vendors[index];
+    }
+
+    public void AddOrder(Order order)
+    {
+      Orders.Add(order);
+    }
+  }
+
+}
